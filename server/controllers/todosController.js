60374344b205fdd87e1ea6db37e83117eb24de27
GreenXred <antonzo09@mail.ru>
2025-10-24 22:00:33 +0300

    Server requests have been prepared and tested:
    - getTodo
    - addTodo
    - deleteTodo

--- server/controllers/todosController.js
+++ server/controllers/todosController.js
@@ -1,21 +1,45 @@
+const TodosModel = require('../models/todosModel');
+
 class TodosController {
-    async getTodos(req, res) {
+    async getTodo(req, res) {
         try {
-            res.send('GET: список задач');
+            const result = await TodosModel.find({}, 'title').exec();
+
+            res.status(200).json({ todos: result });
         } catch (error) {
             res.status(400).json({ message: 'Ошибка при получении задач' });
         }
     }
-    async addTodos(req, res) {
+
+    async addTodo(req, res) {
         try {
-            
+            if (!req.body.title) {
+                res.status(400).json({ message: 'Пожалуйста, добавьте заголовок!' });
+            }
+
+            const todoModel = new TodosModel({ title: req.body.title });
+            await todoModel.save();
+
+            res.status(200).json({ message: 'Задача успешно добавлена!' });
         } catch (error) {
             res.status(400).json({ message: 'Ошибка при добавлении задачи' });
         }
     }
-    async deleteTodos(req, res) { 
+
+    async deleteTodo(req, res) {
         try {
-            
+            const title = req.body && req.body.title;
+            if (!title) {
+                return res.status(400).json({ message: 'Пожалуйста, укажите заголовок!' });
+            }
+
+            const {  deletedCount } = await TodosModel.deleteOne({ title: req.body.title }).exec();
+
+            if (deletedCount === 0) {
+                return res.status(400).json({ message: 'Задача не найдена, проверьте заголовок!' });
+            }
+
+            res.status(200).json({ message: 'Задача успешно удалена!' });
         } catch (error) {
             res.status(400).json({ message: 'Ошибка при удалении задачи' });
         }
--- server/routes/todosRoutes.js
+++ server/routes/todosRoutes.js
@@ -4,11 +4,12 @@ const todosController = require('../controllers/todosController');
 const todosRoutes = new Router(); // Новый роутер для работы с задачами
 
 // Получить все задачи
-todosRoutes.get('/list', todosController.getTodos);
+todosRoutes.get('/list', todosController.getTodo);
 
 // Добавить новую задачу
-todosRoutes.post('/add', todosController.addTodos);
+todosRoutes.post('/add', todosController.addTodo);
 
 // Удалить задачу
-todosRoutes.delete('/delete', todosController.deleteTodos);
+todosRoutes.delete('/delete', todosController.deleteTodo);
+
 module.exports = todosRoutes;
